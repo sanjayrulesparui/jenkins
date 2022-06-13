939d8bb8fc36cd5f309434656bce79b617c92b87
sanjaykumarparui <sanjaykumarparui@localhost>
2022-06-13 13:13:35 -0400

    1

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -32,38 +32,4 @@ pipeline {
     }
 }
 
-def deployFunc(path, env, serverType, ipUser, ipPass) {
-
-    def output
-
-    try {
-        
-        
-        def errMessage = ""
-        def bashPath = path.replaceAll("\\\\", "/")
-        bashPath = bashPath.replaceAll('C:', '/c')
-
-        def dateStr = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new Date())
-
-        echo "\nDeploying mavenized project to ${env} environment... ${dateStr}   ${bashPath}\n"
-        
-        //output = sh label: '', returnStdout: true, script: "cd ${bashPath} && mvn help:system"
-        //print "mvn help:system = ${output}\n"
-        //{ set +x; } 2>/dev/null && 
-        output = sh label: '', returnStdout: true, script: "{ set +x; } 2>/dev/null && cd ${bashPath} && mvn clean package deploy -P deploy-${env}-${serverType}-V4 -D username=${ipUser} -D password=${ipPass} -q"
-        output = output.replaceAll("(?<=password=)(.*)(?= )", "*****")
-
-        echo "mvn deploy -P deploy-${env}-${serverType}-V4 ======================== ${output}\n"
-        
-        dateStr = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new Date())
-        echo "Deployed successfully! ${dateStr}\n"        
-
-        return false
-    } catch (err) {
-        echo "Error while deploying project: ${output}\n"
-        echo "${err}\n"
-        return true
-    }
-}
-
 }
